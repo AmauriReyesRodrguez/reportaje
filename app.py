1b226ed07b6cc929d13991fbe394165dc1478a6d
Amauri Reyes <estudios20xx@gmail.com>
2025-11-01 23:21:33 -0400

    Configurar PostgreSQL en render

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'clave_super_segura_123'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutorial_flask.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
